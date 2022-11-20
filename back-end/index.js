5ba9d83d59eef9134cf12b10cec54e449290bb95
nakulaasimilie <74346294+nakulaasimilie@users.noreply.github.com>
2022-11-20 08:44:09 +0700

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -1,23 +1,22 @@
-const express = require("express");
+const express = require('express');
 const PORT = 2000;
 const server = express();
-const db = require("./models");
-const cors = require("cors")
-const bearerToken = require("express-bearer-token");
+const db = require('./models');
+const cors = require('cors');
+const bearerToken = require('express-bearer-token');
 
-
-require("dotenv").config()
+require('dotenv').config();
 
 server.use(express.json());
 server.use(cors());
-server.use(express.static("./Public"));
+server.use(express.static('./Public'));
 server.use(bearerToken());
 
-const { bookRoutes, userRoutes } = require("./routers");
-server.use("/book", bookRoutes);
-server.use("/user", userRoutes);
+const { bookRoutes, userRoutes } = require('./routers');
+server.use('/book', bookRoutes);
+server.use('/user', userRoutes);
 
 server.listen(PORT, () => {
-    // db.sequelize.sync({ alter: true });
-    console.log("Success Running at PORT: " + PORT);
+  //   db.sequelize.sync({ alter: true });
+  console.log('Success Running at PORT: ' + PORT);
 });
