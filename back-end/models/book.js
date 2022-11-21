bbf05b11d510ca4d7632355015ad157078329a32
nakulaasimilie <74346294+nakulaasimilie@users.noreply.github.com>
2022-11-21 15:28:16 +0700

    done

--- back-end/models/book.js
+++ back-end/models/book.js
@@ -9,6 +9,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Book.hasMany(models.Cart);
+      Book.hasMany(models.Loan_Detail);
     }
   }
   Book.init(
